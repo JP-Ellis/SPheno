ee4bd5b985638d17d704c5fc95aa44530b72779e
JP-Ellis <josh@jpellis.me>
2016-05-05 19:15:41 +1000

    Update version number in code.
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/Control.F90
+++ src/Control.F90
@@ -51,7 +51,7 @@ Integer, Parameter :: qp = Selected_real_kind(25,450)
  !------------------------------------
  ! version number
  !------------------------------------
- Character(len=8), Save :: version="v3.3.8"
+ Character(len=8), Save :: version="v3.4.0"
  !------------------------------------
  ! variables for spectrum calculation
  !------------------------------------
